1ee3f69b6586e43fbf20bebeba35def9610f5535
AshishKharel <ashishkharel58@gmail.com>
2021-04-04 10:58:39 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    let newNames=[]
+    for(let i=0;i<names.length;i++){
+        newNames.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newNames;
+}
+function countDown(i){
+    while(i>-1){
+        console.log(i--);
+    }
+    i;
+}
